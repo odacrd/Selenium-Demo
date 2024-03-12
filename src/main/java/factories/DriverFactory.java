5a33f0c945aba00ec81bc22a6b3acdb6da280456
odacrd <151458201+odacrd@users.noreply.github.com>
2024-03-12 12:24:42 -0400

    added chrome option in DriverFactory to prevent 'save address' or 'save pwd' dialogs.

--- src/main/java/factories/DriverFactory.java
+++ src/main/java/factories/DriverFactory.java
@@ -3,9 +3,13 @@ package factories;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class DriverFactory {
     private static WebDriverWait wait;
@@ -23,7 +27,21 @@ public class DriverFactory {
         return driver;
     }
     private static WebDriver newChromeDriver(){
-        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
+        //ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("chrome.switches", "--disable-extensions --disable-extensions-file-access-check --disable-extensions-http-throttling --disable-infobars --enable-automation --start-maximized");
+
+        HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
+        chromeOptionsMap.put("credentials_enable_service", false);
+        chromeOptionsMap.put("password_manager_enabled", false);
+        chromeOptionsMap.put("autofill.profile_enabled", false);
+        options.setExperimentalOption("prefs", chromeOptionsMap);
+
+
+
+
+
         driver = applyCommonSetup(new ChromeDriver(options));
         return driver;
     }
